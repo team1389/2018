6eb69ad69bc8a3e872fe663197df1fe161458d45
Quunii <kennethmiuraubw@gmail.com>
2018-02-10 16:16:21 -0500

    new inversions

--- src/main/java/com/team1389/robot/RobotMap.java
+++ src/main/java/com/team1389/robot/RobotMap.java
@@ -37,7 +37,7 @@ public class RobotMap
 	protected final boolean sinv_LEFT_DRIVE_T = false;
 	
 	protected final PWM pwm_RIGHT_DRIVE_V = new PWM(0);
-	protected final boolean inv_RIGHT_DRIVE_V = false;
+	protected final boolean inv_RIGHT_DRIVE_V = true;
 	
 	protected final CAN can_RIGHT_DRIVE_T = new CAN(0);
 	protected final boolean inv_RIGHT_DRIVE_T = true;
